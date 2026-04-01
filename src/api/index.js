20fe80e6d2cc05b84544c46e478f4f4967d21433
nik-1207 <nmknikhilkashyap@gmail.com>
2026-04-01 21:50:02 +0530

    feat: add getStores function to retrieve stored items from localStorage

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ export const createStore = (name) => {
     name,
     id: crypto.randomUUID(),
   };
-  const items = localStorage.getItem("stores")
-  localStorage.setItem("stores", JSON.stringify([...JSON.parse(items ?? '[]'), store]));
+  const items = localStorage.getItem("stores");
+  localStorage.setItem("stores", JSON.stringify([...JSON.parse(items ?? "[]"), store]));
+  return store;
 };
+
+export const getStores = () => JSON.parse(localStorage.getItem("stores") ?? "[]");
