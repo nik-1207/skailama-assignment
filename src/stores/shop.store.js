3322e92ba090ac19391246b8a1847a0da3a73f58
nik-1207 <nmknikhilkashyap@gmail.com>
2026-04-01 21:52:33 +0530

    feat: add activeShop state and loadShops method to shop store for managing shop selection

--- src/stores/shop.store.js
+++ src/stores/shop.store.js
@@ -1,5 +1,9 @@
 import { create } from "zustand";
+import { getStores } from "../api";
 
 export const useShopStore = create((set) => ({
+  activeShop: "",
+  shops: [],
   setActiveShop: (shop) => set({ activeShop: shop }),
+  loadShops: () => set({ shops: getStores() }),
 }));
