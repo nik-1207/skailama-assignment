31745cef46eb4799251cbaf975b83814fb51050c
nik-1207 <nmknikhilkashyap@gmail.com>
2026-04-02 19:06:21 +0530

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
-import "./app.module.scss";
+import "./app.scss";
 
 function App() {
   const [count, setCount] = useState(0);
